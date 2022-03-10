34d8d16e11f2e070a5022fecb8701afdb2c7357b
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-03-09 21:26:15 -0800

    Make a book object constructor

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 // script.js
+let myLibrary =[];
+
+function Book(title, author, pages, haveRead)
+{
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
+    this.getInfo = function()
+    {
+        return `title: ${title}, author: ${author}, pages: ${pages}, haveRead: ${haveRead}`;
+    }
+}
