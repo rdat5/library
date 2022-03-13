2c64e38917f71e23a8943aadb612b669ef702990
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-03-12 22:50:23 -0800

    Add library display functionality

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 // script.js
-let myLibrary =[];
 
 function Book(title, author, pages, haveRead)
 {
@@ -12,3 +11,98 @@ function Book(title, author, pages, haveRead)
         return `title: ${title}, author: ${author}, pages: ${pages}, haveRead: ${haveRead}`;
     }
 }
+
+let myLibrary =[];
+// let testBook = new Book("Bepis", "Bippi", 44, false);
+// let testBook2 = new Book("Argh", "Ugh", 99, true);
+
+const bookContainer = document.querySelector('.books');
+const addBookBtn = document.querySelector('.add-book-btn');
+
+function getCardFromBook(book, id)
+{
+    let newCard = document.createElement('article');
+    newCard.setAttribute('class', 'card');
+    newCard.setAttribute('index', id);
+    
+    let newRMBtn = document.createElement('button');
+    newRMBtn.textContent = 'X';
+    newRMBtn.setAttribute('class', 'remove');
+    newRMBtn.addEventListener("click", () =>
+    {
+        onRMBtnClick(id);
+    });
+
+    let newBookTitle = document.createElement('h3');
+    newBookTitle.textContent = book.title;
+    
+    let newBookAuthor = document.createElement('p');
+    newBookAuthor.textContent = book.author;
+    
+    let newBookPages = document.createElement('p');
+    newBookPages.setAttribute('class', 'pages');
+    newBookPages.textContent = book.pages + " pgs";
+    
+    let newBookRead = document.createElement('button');
+    newBookRead.setAttribute('class', 'read-btn');
+    newBookRead.addEventListener("click", () =>
+    {
+        onReadBtnClick(id);
+    });
+    if (book.haveRead)
+    {
+        newBookRead.textContent = "Read";
+    }
+    else
+    {
+        newBookRead.textContent = "Not yet read";
+    }
+
+    
+    newCard.appendChild(newRMBtn);
+    newCard.appendChild(newBookTitle);
+    newCard.appendChild(newBookAuthor);
+    newCard.appendChild(newBookPages);
+    newCard.appendChild(newBookRead);
+
+    return newCard;
+}
+
+function displayBooks()
+{
+    // Clear library display
+    while (bookContainer.firstChild)
+    {
+        bookContainer.removeChild(bookContainer.firstChild);
+    }
+    
+    // Populate libary display from array
+    for (i in myLibrary)
+    {
+        bookContainer.appendChild(getCardFromBook(myLibrary[i], [i]));
+    }
+
+    // If empty tell user
+    if (myLibrary.length === 0)
+    {
+        let emptyText = document.createElement('p');
+        emptyText.textContent = "Library is empty! Add a book.";
+        bookContainer.appendChild(emptyText);
+    }
+}
+
+function onReadBtnClick(id)
+{
+    let clickedBook = myLibrary[id];
+    clickedBook.haveRead = !clickedBook.haveRead;
+    displayBooks();
+}
+
+function onRMBtnClick(id)
+{
+    let clickedBook = myLibrary[id];
+    myLibrary.splice(id, 1);
+    displayBooks();
+}
+
+displayBooks();
